e26dd874434f6bed0a1f7d71a8f18426f412851b
eweix <elliott.weix@icloud.com>
2024-12-19 00:31:05 -0600

    export recipes

--- scripts/generate_pages.py
+++ scripts/generate_pages.py
@@ -7,11 +7,19 @@ with open(r"./data/recipes.yaml", "r") as recipes:
     data = yaml.safe_load(recipes)
     try:
         for recipe in data:
+            # get data for recipes set to be shared
             if "shared" in recipe["categories"]:
                 recipe["categories"].pop("public")
                 recipe["Title"] = recipe["name"]
                 slug = slugify(recipe["name"])
-                with open(r"./content/" + slug + ".md", "w") as page:
+                with open(r"./content/recipes/" + slug + ".md", "w") as page:
                     yaml.safe_dump(recipe, page)
+
+            # alternatively, just generate pages for all recipes
+            # recipe["categories"].pop("public")
+            # recipe["Title"] = recipe["name"]
+            # slug = slugify(recipe["name"])
+            # with open(r"./content/recipes/" + slug + ".md", "w") as page:
+            #     yaml.safe_dump(recipe, page)
     except yaml.YAMLError as out:
         print(out)
