33a863dd12eaf5321a87c0d15f80de2799890593
eweix <elliott.weix@icloud.com>
2024-12-19 00:16:05 -0600

    only publish shared recipes

--- scripts/generate_pages.py
+++ scripts/generate_pages.py
@@ -4,12 +4,14 @@ import yaml
 from slugify import slugify
 
 with open(r"./data/recipes.yaml", "r") as recipes:
-    data = yaml.load(recipes)
+    data = yaml.safe_load(recipes)
     try:
         for recipe in data:
-            recipe["Title"] = recipe["name"]
-            slug = slugify(recipe["name"])
-            with open(r"./content/" + slug + ".md", "w") as page:
-                yaml.dump(recipe, page)
+            if "shared" in recipe["categories"]:
+                recipe["categories"].pop("public")
+                recipe["Title"] = recipe["name"]
+                slug = slugify(recipe["name"])
+                with open(r"./content/" + slug + ".md", "w") as page:
+                    yaml.safe_dump(recipe, page)
     except yaml.YAMLError as out:
         print(out)
