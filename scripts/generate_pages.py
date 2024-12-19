ece6b615b14196d61eb78643d4cc439605bc34b7
eweix <elliott.weix@icloud.com>
2024-12-18 23:53:54 -0600

    include exception for bad syntax

--- scripts/generate_pages.py
+++ scripts/generate_pages.py
@@ -5,10 +5,11 @@ from slugify import slugify
 
 with open(r"./data/recipes.yaml", "r") as recipes:
     data = yaml.load(recipes)
-    for recipe in data:
-        recipe["Title"] = recipe["name"]
-        slug = slugify(recipe["name"])
-        with open(r"./content/" + slug + ".md", "w") as page:
-            yaml.dump(recipe, page)
+    try:
+        for recipe in data:
+            recipe["Title"] = recipe["name"]
+            slug = slugify(recipe["name"])
+            with open(r"./content/" + slug + ".md", "w") as page:
+                yaml.dump(recipe, page)
     except yaml.YAMLError as out:
         print(out)
